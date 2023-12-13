85d8ce05fb6467a0beb3cc6686547ceba384fe21
Michael Babker <michael.babker@gmail.com>
2023-12-12 20:17:51 -0500

    Run CS fixer

--- src/Validator/Constraints/MoneyLessThan.php
+++ src/Validator/Constraints/MoneyLessThan.php
@@ -6,7 +6,6 @@ namespace BabDev\MoneyBundle\Validator\Constraints;
  * Constraint to validate a Money object has a value less than the compared value.
  *
  * @Annotation
- *
  * @Target({"PROPERTY", "METHOD", "ANNOTATION"})
  */
 #[\Attribute(\Attribute::TARGET_PROPERTY | \Attribute::TARGET_METHOD | \Attribute::IS_REPEATABLE)]
